49070562d7fdafd5132cdd0ef873ad271ca74aaa
aurora0043 <940403army@gmail.com>
2025-03-27 10:37:59 +0800

    V1C

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from datetime import datetime, timezone, timedelta
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,17 +16,18 @@ def index():
 
 @app.route("/mis")
 def course():
-	return "<h1>資訊管理導論</h1>"
+    return "<h1>資訊管理導論</h1>"
 
 @app.route("/today")
 def today():
-	tz = timezone(timedelta(hours=8)) 
-    now = datetime.now(tz)
-	return render_template("today.html", datetime = str(now))
+    # 設定時區為 UTC+8
+    tz = timezone(timedelta(hours=8))
+    now = datetime.now(tz)  # 根據 UTC+8 時區獲取當前時間
+    return render_template("today.html", datetime=str(now))  # 傳遞時間給模板
 
 @app.route("/about")
 def me():
-	return render_template("rwd.html")
+    return render_template("rwd.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
@@ -37,12 +39,11 @@ def account():
     if request.method == "POST":
         user = request.form["user"]
         pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd
         return result
     else:
         return render_template("account.html")
 
 
 if __name__ == "__main__":
-	app.run()
-
+    app.run()
